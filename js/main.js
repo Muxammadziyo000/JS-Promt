1c7d1a2775bebdcd4a660a56315a6b930622173e
Muxammadziyo000 <maximjonsen@gmail.com>
2024-02-10 14:12:48 +0500

    j_s-promt

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var num1 = prompt('Ismingiz nima')
+var num2 = prompt('Yoshingiz nechida:')
+var num3 = prompt('telefon raqamingizni kiriting:')
+console.log(num1);
+console.log(num2);
+console.log(num3);
+
